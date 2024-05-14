feda39306a834b2f051b81e7c9f90325818ca747
ttak0422 <ttak0422@gmail.com>
2024-05-15 00:59:11 +0900

    chore: support multiple definitions

--- flake-module.nix
+++ flake-module.nix
@@ -27,80 +27,95 @@ in {
           loaded_tutor_mode_plugin = mkEnableOption "loaded_tutor_mode_plugin";
           loaded_zipPlugin = mkEnableOption "loaded_zipPlugin";
         };
+        opt = {
+          package = mkOption {
+            type = types.package;
+            description = "neovim package";
+            default = pkgs.neovim-unwrapped;
+          };
+        } // runtime // runtime-plugin;
       in {
         options.loaded-nvim = mkOption {
           description = "nvim loaded config";
-          type = types.submodule {
-            options = {
-              package = mkOption {
-                type = types.package;
-                description = "neovim package";
-                default = pkgs.neovim-unwrapped;
-              };
-            } // runtime // runtime-plugin;
-          };
+          type = with types; submodule { options = opt; };
+        };
+        options.loaded-nvim' = mkOption {
+          description = "nvim loaded config (multiple definition support)";
+          type = with types; attrsOf (submodule { options = opt; });
+          default = { };
         };
       });
   };
   config = {
     perSystem = { config, lib, pkgs, ... }:
       let
-        inherit (lib) flatten;
+        inherit (lib) flatten mapAttrs' nameValuePair;
         inherit (lib.strings) concatStringsSep;
 
-        cfg = config.loaded-nvim;
-
-        excludeFiles = flatten
-          ((if cfg.did_load_ftplugin then [ "runtime/ftplugin.vim" ] else [ ])
-            ++ (if cfg.did_indent_on then [ "runtime/indent.vim" ] else [ ])
-            ++ (if cfg.did_install_default_menus then
-              [ "runtime/menu.vim" ]
+        patch = cfg:
+          let
+            excludeFiles = flatten ((if cfg.did_load_ftplugin then
+              [ "runtime/ftplugin.vim" ]
             else
               [ ])
-            ++ (if cfg.skip_loading_mswin then [ "runtime/mswin.vim" ] else [ ])
-            ++ (if cfg.loaded_gzip then [ "runtime/plugin/gzip.vim" ] else [ ])
-            ++ (if cfg.loaded_man then [ "runtime/plugin/man.lua" ] else [ ])
-            ++ (if cfg.loaded_matchit then
-              [ "runtime/plugin/matchit.vim" ]
-            else
-              [ ]) ++ (if cfg.loaded_matchparen then
-                [ "runtime/plugin/matchparen.vim" ]
+              ++ (if cfg.did_indent_on then [ "runtime/indent.vim" ] else [ ])
+              ++ (if cfg.did_install_default_menus then
+                [ "runtime/menu.vim" ]
               else
-                [ ]) ++ (if cfg.loaded_netrwPlugin then
-                  [ "runtime/plugin/netrwPlugin.vim" ]
+                [ ]) ++ (if cfg.skip_loading_mswin then
+                  [ "runtime/mswin.vim" ]
                 else
-                  [ ]) ++ (if cfg.loaded_remote_plugins then
-                    [ "runtime/plugin/rplugin.vim" ]
+                  [ ]) ++ (if cfg.loaded_gzip then
+                    [ "runtime/plugin/gzip.vim" ]
                   else
-                    [ ]) ++ (if cfg.loaded_shada_plugin then
-                      [ "runtime/plugin/shada.vim" ]
+                    [ ])
+              ++ (if cfg.loaded_man then [ "runtime/plugin/man.lua" ] else [ ])
+              ++ (if cfg.loaded_matchit then
+                [ "runtime/plugin/matchit.vim" ]
+              else
+                [ ]) ++ (if cfg.loaded_matchparen then
+                  [ "runtime/plugin/matchparen.vim" ]
+                else
+                  [ ]) ++ (if cfg.loaded_netrwPlugin then
+                    [ "runtime/plugin/netrwPlugin.vim" ]
+                  else
+                    [ ]) ++ (if cfg.loaded_remote_plugins then
+                      [ "runtime/plugin/rplugin.vim" ]
                     else
-                      [ ]) ++ (if cfg.loaded_spellfile_plugin then
-                        [ "runtime/plugin/spellfile.vim" ]
+                      [ ]) ++ (if cfg.loaded_shada_plugin then
+                        [ "runtime/plugin/shada.vim" ]
                       else
-                        [ ]) ++ (if cfg.loaded_tarPlugin then
-                          [ "runtime/plugin/tarPlugin.vim" ]
+                        [ ]) ++ (if cfg.loaded_spellfile_plugin then
+                          [ "runtime/plugin/spellfile.vim" ]
                         else
-                          [ ]) ++ (if cfg.loaded_2html_plugin then
-                            [ "runtime/plugin/tohtml.vim" ]
+                          [ ]) ++ (if cfg.loaded_tarPlugin then
+                            [ "runtime/plugin/tarPlugin.vim" ]
                           else
-                            [ ]) ++ (if cfg.loaded_tutor_mode_plugin then
-                              [ "runtime/plugin/tutor.vim" ]
+                            [ ]) ++ (if cfg.loaded_2html_plugin then
+                              [ "runtime/plugin/tohtml.vim" ]
                             else
-                              [ ]) ++ (if cfg.loaded_zipPlugin then
-                                [ "runtime/plugin/zipPlugin.vim" ]
+                              [ ]) ++ (if cfg.loaded_tutor_mode_plugin then
+                                [ "runtime/plugin/tutor.vim" ]
                               else
-                                [ ]));
-        postInstallCommands =
-          map (target: "rm -f $out/share/nvim/${target}") excludeFiles;
+                                [ ]) ++ (if cfg.loaded_zipPlugin then
+                                  [ "runtime/plugin/zipPlugin.vim" ]
+                                else
+                                  [ ]));
+            postInstallCommands =
+              map (target: "rm -f $out/share/nvim/${target}") excludeFiles;
+          in cfg.package.overrideAttrs (old: {
+            version = "loaded";
+            postInstall = ''
+              ${old.postInstall or ""}
+              ${concatStringsSep "\n" postInstallCommands}
+            '';
+          });
       in {
-        packages.loaded-nvim = cfg.package.overrideAttrs (old: {
-          version = "loaded";
-          postInstall = ''
-            ${if old ? postInstall then old.postInstall else ""}
-            ${concatStringsSep "\n" postInstallCommands}
-          '';
-        });
+        packages = {
+          loaded-nvim = patch config.loaded-nvim;
+        } // (mapAttrs'
+          (name: cfg: nameValuePair "loaded-nvim-${name}" (patch cfg))
+          config.loaded-nvim');
       };
   };
 }
